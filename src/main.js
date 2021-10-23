1f48651becb0067fea43f6fdbb51eeda6e5dc96e
ina <77523846+inhwa-jang@users.noreply.github.com>
2021-10-23 16:27:50 +0900

    Build Pattern GameBuilder

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 'use strict'
 import PopUp from './popup.js';
-import Game from './game.js'
+import GameBuilder from './game.js';
 
 const gameFinishBanner = new PopUp();
-const game = new Game(5, 5, 5);
+const game = new GameBuilder()
+.withGameDuration(5)
+.withCarrotCount(5)
+.withBugCount(7)
+.build();
 
 game.setGameStopListener(reason => {
   console.log(reason);
